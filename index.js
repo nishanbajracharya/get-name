92d005458b6273a8a2c78468b9dd121596d481b7
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-04-02 00:22:19 +0545

    Add JSDoc

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * Return formatted name
+ *
+ * @param {Object} config
+ * @param {String} config.title
+ * @param {String} config.surName
+ * @param {String} config.firstName
+ *
+ * @returns {String}
+ */
 const getName = ({
   title = '',
   surName = '',
