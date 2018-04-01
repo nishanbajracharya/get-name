094418bc8a41019c0c0e0c0e3641adaa8c658a4e
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-04-02 00:11:23 +0545

    Add initial files, mocha and chai for tests

---
+++ index.js
@@ -0,0 +1,5 @@
+const getName = () => {
+  return '';
+}
+
+module.exports = getName;
---
+++ test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+
+const getName = require('./index');
+
+describe('GET NAME', () => {
+  it('should return full name', () => {
+    const input = {
+
+    };
+
+    const expected = '';
+
+    expect(getName(input)).to.equal(expected);
+  });
+});
